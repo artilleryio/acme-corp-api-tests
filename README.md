0f85d856efd3aaee6881c4bec3bb6e2b084fa27b
hveldstra <h@veldstra.org>
2018-11-01 15:44:08 +0000

    Init

---
+++ README.md
@@ -0,0 +1,3 @@
+# acme-corp-api-tests
+
+An example project for testing polyglot microservice-based systems with Artillery (load testing and acceptance/functional/smoke testing).
---
+++ services/auth-service/functions.js
@@ -0,0 +1,5 @@
+'use strict';
+
+// See https://artillery.io/docs/http-reference/#advanced-writing-custom-logic-in-javascript for more details on how we can extend scenarios with custom JS code.
+
+module.exports = {};
